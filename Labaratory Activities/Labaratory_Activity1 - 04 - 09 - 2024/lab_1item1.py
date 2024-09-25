448c4f1ad7b203b4bf596d6814435687d479df63
John Cedrick Deriquito <23-00627@g.batstate-u.edu.ph>
2024-09-25 11:25:15 +0800

    Add files via upload

---
+++ Labaratory Activities/Labaratory_Activity1 - 04 - 09 - 2024/lab_1item1.py
@@ -0,0 +1,17 @@
+year = int(input("Enter the year: "))
+genre = str(input("Enter the genre: "))
+album = str(input("Enter the album: "))
+title = str(input("Enter the song title: "))
+artist = str(input("Enter the artist: "))
+
+print("--------------------")
+print("SONG DETAILS")
+print("--------------------")
+
+print (f"Year Realeased: {year}")
+print (f"Genre: {genre}")
+print (f"Album: {album}")
+print (f"Title: \"{title}\"")
+print (f"Artist: {artist}")
+
+
